bb1d71cf1f2eb7a6121a0f6fa81f952e14545ae0
ousscher <lo_cherguelaine@esi.dz>
2024-02-15 21:07:00 +0100

    added modify content to backend

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -4,5 +4,7 @@ const verify = require ('./verifyUser');
 
 tasksRouter.get('/', verify, tasksController.getAllTasks);
 tasksRouter.post('/add', verify , tasksController.addTask );
+tasksRouter.delete('/delete/:id', verify , tasksController.deleteTask );
+tasksRouter.put('/modify/:id', verify , tasksController.modifyTask );
 
 module.exports = tasksRouter;
