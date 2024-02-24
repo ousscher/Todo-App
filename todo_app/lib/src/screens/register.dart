0a921c2a327acd833466d86fc1b5840e67104644
Cherguelaine Oussama <lo_cherguelaine@esi.dz>
2024-02-24 23:24:31 +0100

    added home page

--- todo_app/lib/src/screens/register.dart
+++ todo_app/lib/src/screens/register.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:todo_app/src/services/auth.dart';
+import 'package:todo_app/src/shared/loading.dart';
 
 class Register extends StatefulWidget {
   Function toogleView;
@@ -22,98 +22,115 @@ class _RegisterState extends State<Register> {
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.width;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Register to the TODO-APP'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Username field
-            TextField(
-              controller: _usernameController,
-              decoration: const InputDecoration(
-                labelText: 'Username',
-                hintText: 'Enter your username',
-              ),
+    return (_loading)
+        ? Loading()
+        : Scaffold(
+            appBar: AppBar(
+              title: const Text('Register to the TODO-APP'),
             ),
-            const SizedBox(height: 20),
-            // Password field
-            TextField(
-              controller: _passwordController,
-              obscureText: true, // For password fields
-              decoration:const InputDecoration(
-                labelText: 'Password',
-                hintText: 'Enter your password',
-              ),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: _confirmPasswordController,
-              obscureText: true, // For password fields
-              decoration: const InputDecoration(
-                labelText: 'Confirm Password',
-                hintText: 'Confirm your password',
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                if (_usernameController.text.isNotEmpty) {
-                  if (_passwordController.text.length < 6) {
-                    setState(() {
-                      _error = "Week password";
-                    });
-                  } else {
-                    if (_passwordController.text ==
-                        _confirmPasswordController.text) {
-                      setState(() {
-                        _error = "";
-                      });
-                    } else {
-                      setState(() {
-                        _error = "Passwords do not match";
-                      });
-                    }
-                  }
-                } else {
-                  setState(() {
-                    _error = "The field of username is empty";
-                  });
-                }
-              },
-              child: const Text('Register'),
-            ),
-            const SizedBox(height: 20),
-            Center(
-              child: Text(
-                _error.isNotEmpty ? _error : "",
-                style: const TextStyle(
-                  color: Colors.red,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 90,
-            ),
-            Center(
-              child: InkWell(
-                onTap: () => widget.toogleView(),
-                child: const Text(
-                  "You already have account? Login here",
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 17,
+            body: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Username field
+                  TextField(
+                    controller: _usernameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Username',
+                      hintText: 'Enter your username',
+                    ),
                   ),
-                ),
+                  const SizedBox(height: 20),
+                  // Password field
+                  TextField(
+                    controller: _passwordController,
+                    obscureText: true, // For password fields
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      hintText: 'Enter your password',
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  TextField(
+                    controller: _confirmPasswordController,
+                    obscureText: true, // For password fields
+                    decoration: const InputDecoration(
+                      labelText: 'Confirm Password',
+                      hintText: 'Confirm your password',
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () async {
+                      if (_usernameController.text.isNotEmpty && _usernameController.text.length>=6 ) {
+                        if (_passwordController.text.length < 6) {
+                          setState(() {
+                            _error = "Week password";
+                          });
+                        } else {
+                          if (_passwordController.text ==
+                              _confirmPasswordController.text) {
+                            setState(() {
+                              _error = "";
+                              _loading = true;
+                            });
+                            try {
+                              String token = await Auth.signupAndLogin(
+                                  _usernameController.text,
+                                  _passwordController.text);
+                              setState(() {
+                                _loading = false;
+                              });
+                              print(token);
+                            } catch (e) {
+                              setState(() {
+                                _error = e.toString();
+                                _loading = false;
+                              });
+                            }
+                          } else {
+                            setState(() {
+                              _error = "Passwords do not match";
+                            });
+                          }
+                        }
+                      } else {
+                        setState(() {
+                          _error = "The field of username is empty";
+                        });
+                      }
+                    },
+                    child: const Text('Register'),
+                  ),
+                  const SizedBox(height: 20),
+                  Center(
+                    child: Text(
+                      _error.isNotEmpty ? _error : "",
+                      style: const TextStyle(
+                        color: Colors.red,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 90,
+                  ),
+                  Center(
+                    child: InkWell(
+                      onTap: () => widget.toogleView(),
+                      child: const Text(
+                        "You already have account? Login here",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 17,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
               ),
-            )
-          ],
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
--- todo_app/lib/src/services/auth.dart
+++ todo_app/lib/src/services/auth.dart
@@ -1,20 +1,57 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-Future<void> login(String username, String password) async {
-  const String apiUrl = 'http://192.168.103.89:3000/api/auth/login';
-  try {
-    print(username+"    "+password);
-    final response = await http.post(
-      Uri.parse(apiUrl),
-      body: {'username': username, 'password': password},
-    );
-    if (response.statusCode == 200) {
-      print("Login succesful");
-      print(response);
-    } else {
-      print("failed");
-    }
-  } catch (e) {
-    print(e);
+class Auth {
+  static String ipAdress = "192.168.176.89";
+  static Future<String> login(String username, String password) async {
+    final String apiUrl = 'http://$ipAdress:3000/api/auth/login';
+
+      final response = await http.post(
+        Uri.parse(apiUrl),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(
+          {
+            'username': username,
+            'password': password,
+          },
+        ),
+      );
+
+      if (response.statusCode == 200) {
+        print('Login successful');
+        String token = jsonDecode(response.body)['token'];
+        print(token);
+        return token;
+      } else {
+        print('Login failed. Status code: ${response.statusCode}');
+        throw Exception('${jsonDecode(response.body)['message']}');
+      }
+  }
+
+  static Future<String> signupAndLogin(String username, String password) async {
+    final String apiUrlSignup = 'http://$ipAdress:3000/api/auth/register';
+
+      final responseSignup = await http.post(
+        Uri.parse(apiUrlSignup),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(
+          {
+            'username': username,
+            'password': password,
+          },
+        ),
+      );
+
+      if (responseSignup.statusCode == 200) {
+        print('Signup successful');
+        // Call login after successful signup
+        return await login(username, password);
+      } else {
+        throw Exception('${jsonDecode(responseSignup.body)['message']}');
+      }
   }
 }
